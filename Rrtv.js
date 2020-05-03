407358fecc8f15b16e1186aae8b7371c5dda8a7c
jiihsu <41851202+jiihsu@users.noreply.github.com>
2020-05-04 01:04:23 +0800

    Update Rrtv.js

--- Rrtv.js
+++ Rrtv.js
@@ -3,7 +3,7 @@
 [MITM]
 hostname = api.rr.tv
 [Script]
-http-response ^https:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll|\/rrtv-video\/v4plus\/season\/detail) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/jiihsu/Surge-script/master/Rrtv.js,script-update-interval=0
+http-response ^https:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll|\/rrtv-video\/v4plus\/season\/detail) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/jiihsu/Surge/master/Rrtv.js,script-update-interval=0
 */
 
 var body = $response.body;
